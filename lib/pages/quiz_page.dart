a23d44bbbd060a2e1442c580d9abd577cd20f5a8
Uday Singh <f71uday@gmail.com>
2024-07-25 16:36:28 +0530

    MOdify for file system and subject page

---
+++ lib/pages/quiz_page.dart
@@ -0,0 +1,248 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_2/pages/subject_list_page.dart';
+import 'package:flutter_application_2/pages/signin_page.dart';
+import 'package:http/http.dart' as http;
+
+import '../Questions/Question.dart';
+ // Ensure you have your model files in the correct directory
+
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  int currentQuestionIndex = 0;
+  int score = 0;
+  int? selectedOptionIndex;
+  Timer? _timer;
+  Duration _remainingTime = Duration(minutes: 10);
+  late Future<List<Question>> questions;
+
+  @override
+  void initState() {
+    super.initState();
+    questions = fetchQuestions();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_remainingTime.inSeconds > 0) {
+          _remainingTime = _remainingTime - Duration(seconds: 1);
+        } else {
+          timer.cancel();
+          _showTimeUpDialog();
+        }
+      });
+    });
+  }
+
+  void _showTimeUpDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Time is up!'),
+        content: FutureBuilder<List<Question>>(
+          future: questions,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Text('No questions available.');
+            } else {
+              return Text('Your score: $score/${snapshot.data!.length}');
+            }
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _formattedTime(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String minutes = twoDigits(duration.inMinutes.remainder(60));
+    String seconds = twoDigits(duration.inSeconds.remainder(60));
+    return '$minutes:$seconds';
+  }
+
+  void _goToQuestion(int index) {
+    setState(() {
+      currentQuestionIndex = index;
+      selectedOptionIndex = null;
+    });
+    Navigator.pop(context); // Close the drawer
+  }
+
+  void _nextQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+      selectedOptionIndex = null;
+    });
+  }
+
+  void _skipQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+      selectedOptionIndex = null;
+    });
+  }
+
+  Future<List<Question>> fetchQuestions() async {
+    final response = await http.get(Uri.parse('http://localhost/questions?page=0&size=20'));
+
+    if (response.statusCode == 200) {
+      return parseQuestions(response.body);
+    } else {
+      throw Exception('Failed to load questions');
+    }
+  }
+
+  List<Question> parseQuestions(String jsonString) {
+    final parsed = json.decode(jsonString);
+    final questionsJson = parsed['_embedded']['questions'] as List;
+    return questionsJson.map((json) => Question.fromJson(json)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Question>>(
+      future: questions,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            appBar: AppBar(title: Text('Quiz App')),
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            appBar: AppBar(title: Text('Quiz App')),
+            body: Center(child: Text('Error: ${snapshot.error}')),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Scaffold(
+            appBar: AppBar(title: Text('Quiz App')),
+            body: Center(child: Text('No questions available.')),
+          );
+        } else {
+          List<Question> questions = snapshot.data!;
+          if (currentQuestionIndex >= questions.length) {
+            return Scaffold(
+              appBar: AppBar(title: Text('Quiz App')),
+              body: Center(child: Text('Your score: $score/${questions.length}')),
+            );
+          }
+
+          Question currentQuestion = questions[currentQuestionIndex];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                _formattedTime(_remainingTime),
+                style: TextStyle(fontSize: 24.0, color: Colors.red),
+              ),
+            ),
+            drawer: Drawer(
+              child: ListView(
+                children: [
+                  DrawerHeader(
+                    child: Text(
+                      'Quiz App',
+                      style: TextStyle(fontSize: 30.0),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Total Questions: ${questions.length}'),
+                  ),
+                  ListTile(
+                    title: Text('Questions Attended: $currentQuestionIndex'),
+                  ),
+                  Divider(),
+                  ...questions.asMap().entries.map((entry) {
+                    int index = entry.key;
+                    return ListTile(
+                      title: Text('Question ${index + 1}'),
+                      onTap: () => _goToQuestion(index),
+                    );
+                  }).toList(),
+                ],
+              ),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${currentQuestionIndex}. ' + currentQuestion.text,
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                  SizedBox(height: 20.0),
+                  ...currentQuestion.options.asMap().entries.map((option) {
+                    final isSelected = option.key == selectedOptionIndex;
+                    final isCorrect = option.value.text == currentQuestion.answer.text;
+                    final color = isSelected ? (isCorrect ? Colors.green : Colors.red) : Colors.black;
+                    return ListTile(
+                      title: Text(
+                        option.value.text,
+                        style: TextStyle(color: color),
+                      ),
+                      leading: Radio<int>(
+                        value: option.key,
+                        groupValue: selectedOptionIndex,
+                        onChanged: (value) {
+                          setState(() {
+                            selectedOptionIndex = value;
+                          });
+                        },
+                      ),
+                    );
+                  }).toList(),
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: selectedOptionIndex == null ? null : _nextQuestion,
+                          child: Text('Next'),
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.all(10.0)),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: _skipQuestion,
+                          child: Text('Skip'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
--- lib/pages/subject_list_page.dart
+++ lib/pages/subject_list_page.dart
@@ -2,9 +2,10 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/main.dart';
+import 'package:flutter_application_2/pages/quiz_page.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/subjects.dart';
-import 'package:http/http.dart' as http;
 
 Future<List<Subject>> fetchSubjects() async {
   final response = await http.get(Uri.parse('http://localhost/subjects'));
@@ -18,12 +19,12 @@ Future<List<Subject>> fetchSubjects() async {
   }
 }
 
-class SubjectListPage extends StatefulWidget{
+class SubjectListPage extends StatefulWidget {
   @override
   _SubjectListPageState createState() => _SubjectListPageState();
 }
 
-class _SubjectListPageState extends State<StatefulWidget>{
+class _SubjectListPageState extends State<SubjectListPage> {
   late Future<List<Subject>> subjects;
 
   @override
@@ -32,41 +33,58 @@ class _SubjectListPageState extends State<StatefulWidget>{
     subjects = fetchSubjects();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Subjects')),
+      body: FutureBuilder<List<Subject>>(
+        future: subjects,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                final subject = snapshot.data![index];
+                return Card(
+                  margin: EdgeInsets.all(10.0),
+                  elevation: 1.0,
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          subject.name,
+                          style: TextStyle(
+                            fontSize: 20.0,
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(title: Text('Subjects')),
-        body: FutureBuilder<List<Subject>>(
-          future: subjects,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data?.length ?? 0,
-                itemBuilder: (context, index) {
-                  final subject = snapshot.data![index];
-                  return ListTile(
-                    title: Text(subject.name),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => QuizPage(),
+                          ),
                         ),
-                      );
-                    },
-                  );
-                },
-              );
-            }
-          },
-        ),
-      );
-    }
-
-  
+                        SizedBox(height: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => QuizPage(),
+                              ),
+                            );
+                          },
+                          child: Text('Start Quiz'),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
 }
