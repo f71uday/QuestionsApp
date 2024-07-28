e7a9305ed16f01476e668b074ec7489bc766cc94
Uday Singh <f71uday@gmail.com>
2024-07-28 17:07:25 +0530

    Toast notification on successfull log in

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,15 +1,51 @@
 import 'package:VetScholar/pages/subject_list_page.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import '../auth/auth_service.dart';
 
 
 class AuthPage extends StatefulWidget {
+
   @override
   _AuthPageState createState() => _AuthPageState();
 }
 
 class _AuthPageState extends State<AuthPage> {
+  late FToast fToast;
   String _userInfo = 'Not Logged In';
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
+  _showCustomToast() {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.lightGreen,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.check, color: Colors.white),
+          SizedBox(
+            width: 12.0,
+          ),
+          Text("Logged in Successfully"),
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.BOTTOM,
+      toastDuration: Duration(seconds: 2),
+    );
+  }
 
   Future<void> _login() async {
     final userInfo = await AuthService.login();
@@ -18,9 +54,9 @@ class _AuthPageState extends State<AuthPage> {
       if (userInfo!= null ) {
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => SubjectListPage()),
-
+          MaterialPageRoute(builder: (context) => SubjectListPage())
         );
+        _showCustomToast();
       }
     });
   }
