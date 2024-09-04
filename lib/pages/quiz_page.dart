e8bcd84eb43919554c70df9723977931e22caeb3
Uday Singh <f71uday@gmail.com>
2024-09-04 23:46:37 +0530

    Update quiz_page.dart

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:VetScholar/pages/view/quiz_result.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:json_theme/json_theme.dart';
 
 import '../models/Questions/question.dart';
 import '../models/Questions/QuestionResponse.dart';
@@ -37,13 +36,13 @@ class _QuizPageState extends State<QuizPage> {
 
   late final QuestionService _questionService;
   List<QuestionResponse> response = [];
+  bool _showPreview = true; // Flag to show quiz preview
 
   @override
   void initState() {
     super.initState();
     _questionService = QuestionService(context);
     questions = _questionService.fetchQuestions(widget.questionLink);
-    _startTimer();
   }
 
   @override
@@ -51,6 +50,13 @@ class _QuizPageState extends State<QuizPage> {
     isActive = false; // set the flag to false when disposing
     _timer?.cancel();
     super.dispose();
+  }
+
+  void _startQuiz() {
+    setState(() {
+      _showPreview = false; // Hide preview and start the quiz
+      _startTimer();
+    });
   }
 
   void _startTimer() {
@@ -198,183 +204,263 @@ class _QuizPageState extends State<QuizPage> {
     }
   }
 
-  bool isPageDisposed() {
-    return !mounted;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Question>>(
-      future: questions,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Scaffold(
-            appBar: AppBar(title: Text('Quiz App')),
-            body: Center(child: CircularProgressIndicator()),
-          );
-        } else if (snapshot.hasError) {
-          return Scaffold(
-            appBar: AppBar(title: Text('Quiz App')),
-            body: Center(child: Text('Error: ${snapshot.error}')),
-          );
-        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return Scaffold(
-            appBar: AppBar(title: const Text('Quiz App')),
-            body: const Center(child: Text('No questions available.')),
-          );
-        } else {
-          List<Question> questions = snapshot.data!;
-          if (currentQuestionIndex >= questions.length) {
-            // Navigate to the ColorAnimationPage when the quiz ends
-            return ColorAnimationPage(
-              response: response,
-              link: _questionService.getResponseLink(),
-            );
-          }
-
-          Question currentQuestion = questions[currentQuestionIndex];
-
-          return Scaffold(
-            key: _scaffoldKey,
-            appBar: AppBar(
-              title: Text(
-                _formattedTime(_remainingTime),
-                style: TextStyle(fontSize: 24.0, color: _getTimerTextColor()),
-              ),
-              elevation: 5.0,
-              actions: [
-                IconButton(
-                  icon: Icon(Icons.info_outline),
-                  onPressed: () {
-                    _showBottomSheet(currentQuestion);
-                  },
-                )
-              ],
-            ),
-            drawer: Drawer(
-              child: Column(
-                children: [
-                  DrawerHeader(
-                    child: Column(
-                      children: [
-                        Text(
-                          '${_questionService.getQuizName()} Quiz',
-                          style: const TextStyle(fontSize: 30.0),
-                        ),
-                      ],
-                    ),
-                  ),
-                  ListTile(
-                    title: Text('Total Questions: ${questions.length}'),
-                  ),
-                  ListTile(
-                    title: Text('Questions Attended: $currentQuestionIndex'),
-                  ),
-                  Divider(),
-                  Expanded(
-                    child: GridView.builder(
-                      padding: EdgeInsets.all(10),
-                      shrinkWrap: true,
-                      physics: const AlwaysScrollableScrollPhysics(),
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 10,
+    if (_showPreview) {
+      return Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: _startQuiz,
+            child: const Icon(Icons.arrow_forward),
+          ),
+          appBar: AppBar(
+            title: Text('Preview'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: FutureBuilder<List<Question>>(
+              future: questions,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Text('No questions available.');
+                } else {
+                  final questionCount = snapshot.data!.length;
+                  return Column(
+                    children: [
+                      Text(
+                        _questionService.getQuizName(),
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
                       ),
-                      itemCount: questions.length,
-                      itemBuilder: (context, index) {
-                        final color = questions[index].isAnswered
-                            ? Colors.green
-                            : Colors.red;
-                        return GestureDetector(
-                          onTap: () => _goToQuestion(index),
-                          child: Card(
-                            color: color,
-                            child: Center(
-                              child: Text(
-                                '${index + 1}',
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ),
+                      SizedBox(
+                        height: 60,
+                      ),
+                      SizedBox(height: 15),
+                      const Row(
+                        children: [
+                          Text("Specifications"),
+                          SizedBox(
+                            width: 8,
                           ),
-                        );
-                      },
-                    ),
-                  ),
-                  Divider(),
-                  //Padding(padding: EdgeInsets.all(8)),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: FilledButton(
-                          onPressed: _confirmEnd,
-                          child: const Text(
-                            'End',
-                          )),
-                    ),
+                          Expanded(
+                              child: Divider(
+                                color: Colors.grey,
+                                thickness: 1,
+                              )),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(Icons.question_mark_outlined),
+                          SizedBox(width: 10,),
+                          Text('Total Questions ',style: TextStyle(fontSize: 16),),
+                          Spacer(),
+                          Text('$questionCount', style: TextStyle(fontSize: 16),),
+
+                        ],
+                      ),
+                      SizedBox(height: 20,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(Icons.timer_outlined),
+                          SizedBox(width: 10,),
+                          Text('Total Time ',style: TextStyle(fontSize: 16),),
+                          Spacer(),
+                          Text('${_formattedTime(_remainingTime)} Min', style: TextStyle(fontSize: 16),),
+
+                        ],
+                      ),
+
+                    ],
+                  );
+                }
+              },
+            ),
+          ));
+    }
+
+    return PopScope(
+      canPop: false,
+      child: FutureBuilder<List<Question>>(
+        future: questions,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(
+              appBar: AppBar(title: Text('Quiz App')),
+              body: Center(child: CircularProgressIndicator()),
+            );
+          } else if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(title: Text('Quiz App')),
+              body: Center(child: Text('Error: ${snapshot.error}')),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Scaffold(
+              appBar: AppBar(title: const Text('Quiz App')),
+              body: const Center(child: Text('No questions available.')),
+            );
+          } else {
+            List<Question> questions = snapshot.data!;
+            if (currentQuestionIndex >= questions.length) {
+              // Navigate to the ColorAnimationPage when the quiz ends
+              return ColorAnimationPage(
+                response: response,
+                link: _questionService.getResponseLink(),
+              );
+            }
+
+            Question currentQuestion = questions[currentQuestionIndex];
+
+            return Scaffold(
+              key: _scaffoldKey,
+              appBar: AppBar(
+                title: Text(
+                  _formattedTime(_remainingTime),
+                  style: TextStyle(fontSize: 24.0, color: _getTimerTextColor()),
+                ),
+                elevation: 5.0,
+                actions: [
+                  IconButton(
+                    icon: Icon(Icons.info_outline),
+                    onPressed: () {
+                      _showBottomSheet(currentQuestion);
+                    },
                   )
                 ],
               ),
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '${currentQuestionIndex + 1}. ' + currentQuestion.question,
-                    style: TextStyle(fontSize: 24.0),
-                  ),
-                  SizedBox(height: 20.0),
-                  ...currentQuestion.options.asMap().entries.map((option) {
-                    final isSelected = option.key == selectedOptionIndex;
-                    final isCorrect =
-                        option.value.text == currentQuestion.answer.text;
-
-                    return ListTile(
-                      title: Text(
-                        option.value.text,
+              drawer: Drawer(
+                child: Column(
+                  children: [
+                    DrawerHeader(
+                      child: Column(
+                        children: [
+                          Text(
+                            '${_questionService.getQuizName()} Quiz',
+                            style: const TextStyle(fontSize: 30.0),
+                          ),
+                        ],
                       ),
-                      leading: Radio<int>(
-                        value: option.key,
-                        groupValue: selectedOptionIndex,
-                        onChanged: (value) {
-                          setState(() {
-                            selectedOptionIndex = value;
-                          });
+                    ),
+                    ListTile(
+                      title: Text('Total Questions: ${questions.length}'),
+                    ),
+                    ListTile(
+                      title: Text('Questions Attended: $currentQuestionIndex'),
+                    ),
+                    Divider(),
+                    Expanded(
+                      child: GridView.builder(
+                        padding: EdgeInsets.all(10),
+                        shrinkWrap: true,
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                        ),
+                        itemCount: questions.length,
+                        itemBuilder: (context, index) {
+                          final color = questions[index].isAnswered
+                              ? Colors.green
+                              : Colors.red;
+                          return GestureDetector(
+                            onTap: () => _goToQuestion(index),
+                            child: Card(
+                              color: color,
+                              child: Center(
+                                child: Text(
+                                  '${index + 1}',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            ),
+                          );
                         },
                       ),
-                    );
-                  }).toList(),
-                  Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: FilledButton.tonal(
-                          onPressed: _skipQuestion,
-                          child: Text('Skip'),
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.all(10.0)),
-                      Expanded(
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: SizedBox(
+                        width: double.infinity,
                         child: FilledButton(
-                          onPressed: selectedOptionIndex == null
-                              ? null
-                              : _nextQuestion,
-                          child: Text('Next'),
-                        ),
+                            onPressed: _confirmEnd,
+                            child: const Text(
+                              'End',
+                            )),
                       ),
-                    ],
-                  ),
-                ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          );
-        }
-      },
+              body: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '${currentQuestionIndex + 1}. ' +
+                          currentQuestion.question,
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                    SizedBox(height: 20.0),
+                    ...currentQuestion.options.asMap().entries.map((option) {
+                      final isSelected = option.key == selectedOptionIndex;
+                      final isCorrect =
+                          option.value.text == currentQuestion.answer.text;
+
+                      return ListTile(
+                        title: Text(
+                          option.value.text,
+                        ),
+                        leading: Radio<int>(
+                          value: option.key,
+                          groupValue: selectedOptionIndex,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedOptionIndex = value;
+                            });
+                          },
+                        ),
+                      );
+                    }).toList(),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: FilledButton.tonal(
+                            onPressed: _skipQuestion,
+                            child: Text('Skip'),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.all(10.0)),
+                        Expanded(
+                          child: FilledButton(
+                            onPressed: selectedOptionIndex == null
+                                ? null
+                                : _nextQuestion,
+                            child: Text('Next'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 
