d7c7fa5356be477cd4ac6c7e0c7acdb1e627a7b5
Uday Singh <f71uday@gmail.com>
2024-09-24 17:05:17 +0530

    Update bookmarked_questions.dart

--- lib/pages/bookmarked_questions.dart
+++ lib/pages/bookmarked_questions.dart
@@ -1,7 +1,10 @@
 import 'package:VetScholar/models/test_result/question_responses.dart';
+import 'package:VetScholar/service/bookmark_service.dart';
+import 'package:VetScholar/service/context_utility.dart';
 import 'package:VetScholar/service/question_service.dart';
 import 'package:flutter/material.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'error/no-intrnet.dart';
 
 class BookmarkedQuestionsPage extends StatefulWidget {
@@ -17,12 +20,12 @@ class BookmarkedQuestionsPageState extends State<BookmarkedQuestionsPage>
   bool _isLoading = true;
   bool _hasError = false;
   final RefreshController _refreshController =
-      RefreshController(initialRefresh: false);
+  RefreshController(initialRefresh: false);
   int _currentPage = 0;
   bool _isLastPage = false;
 
   // To track expansion state
-  List<bool> _isExpandedList = [];
+  Set<int> _expandedTiles = Set<int>();
 
   @override
   void initState() {
@@ -41,9 +44,6 @@ class BookmarkedQuestionsPageState extends State<BookmarkedQuestionsPage>
     setState(() {
       _bookmarked = testResults!;
       _isLoading = false;
-
-      // Initialize the expansion state list with 'false' values (collapsed)
-      _isExpandedList = List.filled(_bookmarked.length, false);
     });
   }
 
@@ -57,7 +57,7 @@ class BookmarkedQuestionsPageState extends State<BookmarkedQuestionsPage>
     try {
       QuestionService services = QuestionService(context);
       List<QuestionResponses> newResults =
-          await services.getBookMarked(page: _currentPage);
+      await services.getBookMarked(page: _currentPage);
       if (newResults.isNotEmpty) {
         if (isRefresh) {
           _bookmarked = newResults;
@@ -73,8 +73,8 @@ class BookmarkedQuestionsPageState extends State<BookmarkedQuestionsPage>
           _refreshController.refreshCompleted();
           _currentPage = 2; // Reset to the next page
         }
-        // Update the expansion state list after loading new data
-        _isExpandedList = List.filled(_bookmarked.length, false);
+        // Initialize or update expansion state list
+        _expandedTiles = Set<int>();
       } else {
         _isLastPage = true;
         if (isLoadMore) {
@@ -94,6 +94,128 @@ class BookmarkedQuestionsPageState extends State<BookmarkedQuestionsPage>
     }
   }
 
+  Widget _buildQuestionTile(
+      QuestionResponses questionResponse, int index, bool isExpanded) {
+    return Slidable(
+      key: ValueKey(index),
+      startActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {
+              _toggleBookmark(_bookmarked,index);
+            },
+            backgroundColor: Colors.orange,
+            foregroundColor: Colors.white,
+            icon: Icons.bookmark,
+            label: 'Remove Bookmark',
+          ),
+        ],
+      ),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {
+              // Action for delete
+            },
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.flag_outlined,
+            label: 'Flag',
+          ),
+        ],
+      ),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (isExpanded) {
+              _expandedTiles.remove(index);
+            } else {
+              _expandedTiles.add(index);
+            }
+          });
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              contentPadding:
+              const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              title: Text(
+                questionResponse.question.text,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+
+                  Flexible(
+                    child: Icon(
+                      isExpanded ? Icons.expand_less : Icons.expand_more,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            AnimatedSize(
+              duration: const Duration(milliseconds: 300),
+              reverseDuration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOutBack,
+              child: _expandedTiles.contains(index)
+                  ? SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'Answer: ',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .primary),
+                          ),
+                          Expanded(
+                            child: Text(
+                              questionResponse.question.answer.text ??
+                                  'No Answer Available',
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Wrap(
+                        spacing: 8.0,
+                        runSpacing: 8.0,
+                        children: questionResponse.question.topics
+                            .map((tag) => Chip(
+                          label: Text(tag.name),
+                        ))
+                            .toList(),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+                  : Container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -104,59 +226,44 @@ class BookmarkedQuestionsPageState extends State<BookmarkedQuestionsPage>
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
           : _hasError
-              ? NoInternetPage(
-                  onRetry: () {
-                    setState(() {
-                      _isLoading = true;
-                      _hasError = false;
-                    });
-                    _fetchBookMarkedQuestions();
-                  },
-                )
-              : SmartRefresher(
-                  controller: _refreshController,
-                  onRefresh: () => _fetchBookMarkedQuestions(isRefresh: true),
-                  enablePullUp: true,
-                  onLoading: () => _fetchBookMarkedQuestions(isLoadMore: true),
-                  child: SingleChildScrollView(
-                    child: ExpansionPanelList(
-                      expansionCallback: (int index, bool isExpanded) {
-                        setState(() {
-                          _isExpandedList[index] = isExpanded;
-                        });
-                      },
-                      children: _bookmarked
-                          .asMap()
-                          .entries
-                          .map<ExpansionPanel>((entry) {
-                        int index = entry.key;
-                        QuestionResponses bookmarked = entry.value;
-                        return
-                          ExpansionPanel(
-                          headerBuilder:
-                              (BuildContext context, bool isExpanded) {
-                            return ListTile(
-                              title: Text(
-                                bookmarked.question.text,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                ),
-                              ),
-                            );
-                          },
-                          body: ListTile(
-                            title: Text(bookmarked.question.answer.text ??
-                                'No Answer Available'),
-                          ),
-                          isExpanded: _isExpandedList[index],
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                ),
+          ? NoInternetPage(
+        onRetry: () {
+          setState(() {
+            _isLoading = true;
+            _hasError = false;
+          });
+          _fetchBookMarkedQuestions();
+        },
+      )
+          : SmartRefresher(
+        controller: _refreshController,
+        onRefresh: () => _fetchBookMarkedQuestions(isRefresh: true),
+        enablePullUp: true,
+        onLoading: () => _fetchBookMarkedQuestions(isLoadMore: true),
+        child: ListView.separated(
+          separatorBuilder: (context, index) {
+            return const Divider(height: 1.0);
+          },
+          itemCount: _bookmarked.length,
+          itemBuilder: (context, index) {
+            final bookmarked = _bookmarked[index];
+            bool isExpanded = _expandedTiles.contains(index);
+            return _buildQuestionTile(bookmarked, index, isExpanded);
+          },
+        ),
+      ),
     );
   }
+
+  void _toggleBookmark(List<QuestionResponses> questionResponse, int index) {
+    // Your implementation for toggling bookmark
+    setState(() {
+      questionResponse.removeAt(index);
+    });
+    BookmarkService(ContextUtility.context!).removeBookmark(questionResponse[index].question.id);
+  }
+
+
 
   @override
   void dispose() {
